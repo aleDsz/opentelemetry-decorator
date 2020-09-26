e8d995d3792ab2cfc89282ea8ea5f08a15cfdf1a
Alexandre de Souza <alexandre@aledsz.com.br>
2020-09-26 00:01:51 -0300

    Add decorator for OpenTelemetry

--- lib/open_telemetry_decorator.ex
+++ lib/open_telemetry_decorator.ex
@@ -1,18 +1,51 @@
 defmodule OpenTelemetryDecorator do
   @moduledoc """
-  Documentation for OpenTelemetryDecorator.
+  Implement decorators to your functions and send data to OpenTelemetry automatically
   """
+  use Decorator.Define, with_span: 0
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> OpenTelemetryDecorator.hello()
-      :world
-
+  Decorate function to run OpenTelemetry.Tracer.with_span/2
   """
-  def hello do
-    :world
+  def with_span(block, context) do
+    span_name = get_span_name(context)
+
+    quote do
+      alias OpenTelemetryDecorator.Error, as: E
+      alias OpenTelemetryDecorator.Exception, as: Ex
+      alias OpenTelemetryDecorator.Result, as: R
+
+      require OpenTelemetry.{Span, Tracer}
+
+      OpenTelemetry.Tracer.with_span unquote(span_name) do
+        try do
+          case unquote(block) do
+            {:ok, result} ->
+              OpenTelemetry.Span.set_attributes(R.handle_result(result))
+              {:ok, result}
+
+            {:error, _} = error ->
+              OpenTelemetry.Span.set_attributes(E.handle_error(error))
+              error
+
+            {:error, _, _, _} = error ->
+              OpenTelemetry.Span.set_attributes(E.handle_error(error))
+              error
+
+            other ->
+              other
+          end
+        rescue
+          exception ->
+            OpenTelemetry.Span.set_attributes(Ex.handle_exception(exception))
+            raise exception
+        end
+      end
+    end
+  end
+
+  @spec get_span_name(Decorator.Decorate.Context.t()) :: binary()
+  defp get_span_name(context = %Decorator.Decorate.Context{}) do
+    "#{context.module}.#{context.name}/#{context.arity}"
   end
 end
