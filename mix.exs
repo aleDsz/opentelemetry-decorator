da7194e74218e6e451420528633b0d78ac8b8853
Alexandre de Souza <alexandre@aledsz.com.br>
2020-09-26 00:00:53 -0300

    Add decorator and opentelemetry_api dependencies)

--- mix.exs
+++ mix.exs
@@ -1,28 +1,24 @@
 defmodule OpenTelemetryDecorator.MixProject do
   use Mix.Project
 
-  def project do
-    [
+  def project,
+    do: [
       app: :opentelemetry_decorator,
       version: "0.1.0",
       elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
-  end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  def application, do: [extra_applications: [:logger]]
+
+  defp deps,
+    do: [
+      {:opentelemetry_api, "~> 0.3.0"},
+      {:decorator, "~> 1.2"}
     ]
-  end
 end
