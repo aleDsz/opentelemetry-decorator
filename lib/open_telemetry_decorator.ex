14ad8215cfc6aea548f8ff7339bbd6a45cf2853a
Alexandre de Souza <alexandre@aledsz.com.br>
2020-10-01 00:18:10 -0300

    Implement custom handler for function results

--- lib/open_telemetry_decorator.ex
+++ lib/open_telemetry_decorator.ex
@@ -11,9 +11,9 @@ defmodule OpenTelemetryDecorator do
     span_name = get_span_name(context)
 
     quote do
-      alias OpenTelemetryDecorator.Error, as: E
-      alias OpenTelemetryDecorator.Exception, as: Ex
-      alias OpenTelemetryDecorator.Result, as: R
+      result_handler = OpenTelemetryDecorator.get_module(:result)
+      error_handler = OpenTelemetryDecorator.get_module(:error)
+      exception_handler = OpenTelemetryDecorator.get_module(:exception)
 
       require OpenTelemetry.{Span, Tracer}
 
@@ -21,15 +21,15 @@ defmodule OpenTelemetryDecorator do
         try do
           case unquote(block) do
             {:ok, result} ->
-              OpenTelemetry.Span.set_attributes(R.handle_result(result))
+              OpenTelemetry.Span.set_attributes(result_handler.handle_result(result))
               {:ok, result}
 
             {:error, _} = error ->
-              OpenTelemetry.Span.set_attributes(E.handle_error(error))
+              OpenTelemetry.Span.set_attributes(error_handler.handle_error(error))
               error
 
             {:error, _, _, _} = error ->
-              OpenTelemetry.Span.set_attributes(E.handle_error(error))
+              OpenTelemetry.Span.set_attributes(error_handler.handle_error(error))
               error
 
             other ->
@@ -38,7 +38,11 @@ defmodule OpenTelemetryDecorator do
         rescue
           exception ->
             stacktrace = __STACKTRACE__
-            OpenTelemetry.Span.set_attributes(Ex.handle_exception(exception, stacktrace))
+
+            OpenTelemetry.Span.set_attributes(
+              exception_handler.handle_exception(exception, stacktrace)
+            )
+
             reraise exception, stacktrace
         end
       end
@@ -48,5 +52,30 @@ defmodule OpenTelemetryDecorator do
   @spec get_span_name(Decorator.Decorate.Context.t()) :: binary()
   defp get_span_name(context = %Decorator.Decorate.Context{}) do
     "#{context.module}.#{context.name}/#{context.arity}"
+  end
+
+  @doc """
+  Get module from config to get attributes to use on OpenTelemetry
+
+  You can input in your `config/*.exs`:
+
+  ### Example
+      config :opentelemetry_decorator, handler: MyApp.CustomHandler
+  """
+  @spec get_module(:error | :result | :exception) :: module()
+  def get_module(:error) do
+    get_module_from_config(OpenTelemetryDecorator.Error)
+  end
+
+  def get_module(:result) do
+    get_module_from_config(OpenTelemetryDecorator.Result)
+  end
+
+  def get_module(:exception) do
+    get_module_from_config(OpenTelemetryDecorator.Exception)
+  end
+
+  defp get_module_from_config(default) do
+    Application.get_env(:opentelemetry_decorator, :handler, default)
   end
 end
